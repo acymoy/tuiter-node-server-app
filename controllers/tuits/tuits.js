74de470cf876567cdab2598a63faad0eef490fe4
Andrew Moy <amoy@umd.edu>
2023-04-02 22:47:17 -0400

    updated tuits

--- controllers/tuits/tuits.js
+++ controllers/tuits/tuits.js
@@ -1,23 +1,50 @@
 export default [
     {
-     "_id": "123", "topic": "Space", "username": "SpaceX",
-     "handle": "@spacex", "time": "2h", "image": "spacex.png",
-     "title": "SpaceX's Mission",
-     "tuit": "You want to wake up in the morning and think the future is going to be great - and that’s what being a spacefaring civilization is all about. It’s about believing in the future and thinking that the future will be better than the past. And I can’t think of anything more exciting than going out there and being among the stars",
-     "liked": true, "likes": 2345, "replies": 123, "retuits": 432
+        "userProfile": "https://cdn.britannica.com/05/236505-050-17B6E34A/Elon-Musk-2022.jpg",
+        "userName": "SpaceX",
+        "handle": "spacex",
+        "time": "2h",
+        "text": "You want to wake up in the morning and think the future is going to be great - and that’s what being a spacefaring civilization is all about. It’s about believing in the future and thinking that the future will be better than the past. And I can’t think of anything more exciting than going out there and being among the stars",
+        "mediaThumbnail": "https://flxt.tmsimg.com/assets/p20454835_b_h8_aa.jpg",
+        "comments": 4200,
+        "shares": 3500,
+        "likes": 37500,
+        "liked": true,
+        "_id": 1
+    }, 
+    {
+        "userProfile": "https://pbs.twimg.com/profile_images/1039775185862242304/-NLaPOqV_400x400.jpg",
+        "userName": "The Boring Company",
+        "handle": "@boringcompany",
+        "time": "23h",
+        "text": "The Boring Company (TBC) is an American infrastructure and tunnel construction services company founded by Elon Musk. Its ongoing and proposed projects are designed for intra-city ('loop') transit systems.",
+        "mediaThumbnail": "https://pbs.twimg.com/media/FfdpGlRWYA8Qhm0?format=jpg&name=4096x4096",
+        "mediaTitle": "",
+        "mediaDescription": "",
+        "comments": 965,
+        "shares": 2400,
+        "likes": 4000,
+        "_id": 2
     },
     {
-     "_id": "234", "topic": "Traffic", "username": "The Boring Company",
-     "handle": "@boringcompany", "time": "2h", "image": "boring2.jpg",
-     "title": "The Boring Company fixing traffic",
-     "tuit": "The Boring Company (TBC) is an American infrastructure and tunnel construction services company founded by Elon Musk. Its ongoing and proposed projects are designed for intra-city ('loop') transit systems.",
-     "liked": true, "likes": 2345, "replies": 321, "retuits": 432
-    },
+        "userProfile": "https://pbs.twimg.com/profile_images/895407294636187648/xoppJ-eR_400x400.jpg",
+        "userName": "Neuralink",
+        "handle": "@neuralink",
+        "time": "3h",
+        "text": "Neurolink is an American company founded by Elon Musk that specializes in brain to computer integration. The first application will be to cure spinal cord injuries so that paraplegics can walk again. Other applications include curing blindness and mental disorders.",
+        "mediaThumbnail": "https://pbs.twimg.com/card_img/1641157949916626954/hXG9rMnx?format=jpg&name=small",
+        "mediaTitle": "",
+        "mediaDescription": "",
+        "comments": 965,
+        "shares": 2400,
+        "likes": 4000,
+        "_id": 3
+    }
     {
-     "_id": "345", "topic": "Brain Injuries", "username": "Neurolink",
-     "handle": "@neurolink", "time": "3h", "image": "neurolink.jpg",
+     "_id": "345", "topic": "Brain Injuries", "username": "",
+     "handle": "", "time": "3h", "image": "neurolink.jpg",
      "title": "Neurolink fixes paralisis and blindness",
-     "tuit": "Neurolink is an American company founded by Elon Musk that specializes in brain to computer integration. The first application will be to cure spinal cord injuries so that paraplegics can walk again. Other applications include curing blindness and mental disorders.",
+     "tuit": "",
      "liked": true, "likes": 3456, "replies": 345, "retuits": 654
     },
     ];
